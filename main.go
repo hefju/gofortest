a127566e91369d95758267d7f7410fbff240280a
hefju <hefju@live.cn>
2015-02-23 16:28:53 +0800

    用idea14上传的

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/hefju/gofortest/jutil"
+	"github.com/hefju/gofortest/jutil"//采用gopath+路径, 不知道为什么webstorm不能识别,idea14可以
 )
 
-//该项目用来测试go代码的
+//该项目用来测试go代码的,
 func main() {
 	jutil.ParseIni("conf.ini")
-	fmt.Println(jutil.IniConfiger)
+	fmt.Println(jutil.IniConfiger["server"]["ip"])
 	fmt.Println("end")
 }
