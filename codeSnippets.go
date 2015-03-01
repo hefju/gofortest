f8d563868bc9b8a5063ce7afa0ab39bc6e1fffb0
hefju <hefju@live.cn>
2015-03-01 22:10:01 +0800

    终于实现了热编译了, 虽然效果有所欠缺, 最终还是运行起来了. 根据用户指定的文件, 执行go run命令, 也能使用taskkill命令来杀进程. 使用fsnotify库来监测文件的变化. 自己写文件夹遍历, 把当前目录及所有子目录都加入监测队列里面. 这个热编译的缺点是无法显示目标程序输出的信息.

--- codeSnippets.go
+++ codeSnippets.go
@@ -2,37 +2,162 @@
 package main
 
 import (
-	"fmt"
-	"os/exec"
-	"time"
+//"bytes"
+    "fmt"
+    "github.com/go-fsnotify/fsnotify"
+    "io/ioutil"
+    "log"
+    "os/exec"
+    "path"
+    "time"
 )
-//这是一个独立的程序, 放在myexec.go文件里面. gobuild之后myexec.exe放在myserver.go同一层文件夹下, 就可以启动myserver了
-func main() {
 
-	done := make(chan bool)
-	ticker := time.NewTicker(time.Second)
-	heartbeat := 0
+func main2() {
+    //重启服务器的通道
+    restartChan := make(chan int)
 
-	cmd := exec.Command("go", "run", "myserver.go")
-	cmd.Start()
+    go SetWatcher(".", restartChan)
 
-	go func() {
-		time.Sleep(time.Second * 20)
-		c := exec.Command("taskkill.exe", "/f", "/im", "myserver.exe")
-		err := c.Start()
-		if err != nil {
-			fmt.Println(err)
-		}
-		done <- true
-	}()
+    fmt.Println("runner begin")
+    runner := runner{"myhttp"}
+    runner.Run()
+    runner.WaitForRestart(restartChan)
+    // for {
+    // 	var input string
+    // 	fmt.Scan(&input)
+    // 	if input == "ex" {
+    // 		break
+    // 	} else {
+    // 		fmt.Println("bad cmd")
+    // 	}
+    // }
 
-	go func() {
-		for {
-			<-ticker.C
-			heartbeat++
-			fmt.Println(heartbeat)
-		}
-	}()
-	<-done
-	fmt.Println("myexec.go cao")
 }
+
+type runner struct {
+    filename string
+}
+
+//重启服务器
+func (x runner) WaitForRestart(order chan int) {
+    for {
+        <-order
+        x.Kill()
+        x.Run()
+    }
+}
+
+//执行命令
+func (x runner) Run() {
+    goname := x.filename + ".go"
+    fmt.Println("Run:" + goname)
+    c := exec.Command("go", "run", goname) // "myserver.go")
+
+    c.Start()
+}
+
+//终结进程.
+func (x runner) Kill() {
+    taskname := x.filename + ".exe"
+    fmt.Println("Kill:" + taskname)
+    c := exec.Command("taskkill.exe", "/f", "/im", taskname) //"myserver.exe")
+    err := c.Start()
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func (x runner) Loop() {
+    for {
+        fmt.Println("Loop:" + x.filename)
+        x.Run()
+        go myheartbeat(10)
+        time.Sleep(time.Second * 10)
+
+        x.Kill()
+        go myheartbeat(-5)
+        time.Sleep(time.Second * 5)
+    }
+}
+
+func myheartbeat(count int) {
+    max := 0
+    heartbeat := 0
+    if count > 0 {
+        max = count
+    } else {
+        max = 0 - 1
+        heartbeat = count - 1
+    }
+
+    ticker := time.NewTicker(time.Second)
+    for {
+        <-ticker.C
+        heartbeat++
+        if heartbeat > max {
+            break
+        }
+        out := heartbeat
+        if out < 0 {
+            out = 0 - out
+        }
+        fmt.Println(out)
+    }
+}
+
+var lstFolder []string
+
+func SetWatcher(root string, order chan int) {
+    lstFolder = make([]string, 0)
+    lstFolder = append(lstFolder, root)
+    GetFoleder(root)
+
+    watcher, err := fsnotify.NewWatcher()
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer watcher.Close()
+
+    done := make(chan bool)
+    go func() {
+        for {
+            select {
+            case event := <-watcher.Events:
+            //如果有go文件修改,才触发更新
+                if path.Ext(event.Name) == ".go" && event.Op&fsnotify.Write == fsnotify.Write {
+                    //fmt.Println(event.Name, "--", event.Op, "--", event)
+                    order <- 0
+                }
+
+            case err := <-watcher.Errors:
+                log.Println("error:", err)
+            }
+        }
+    }()
+    fmt.Println(".watcher.") //开始监视
+
+    for _, f := range lstFolder {
+        err = watcher.Add(f)
+        if err != nil {
+            log.Fatal("138:", err)
+        }
+    }
+
+    <-done
+}
+
+func GetFoleder(root string) {
+    files, _ := ioutil.ReadDir(root)
+    for _, fi := range files {
+        if fi.IsDir() {
+            if fi.Name()[0] == '.' { //不处理以.开头的文件夹
+                continue
+            }
+            mypath := path.Join(root, fi.Name())
+            lstFolder = append(lstFolder, mypath)
+            GetFoleder(mypath)
+            //fmt.Println(mypath)
+        }
+    }
+}
+
